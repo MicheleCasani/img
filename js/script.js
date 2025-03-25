0cee44746caab52813fef2970d0f384f6c963507
MicheleCasani <casanimichele@gmail.com>
2025-03-25 14:25:47 +0100

    [DONE] add bonus

--- js/script.js
+++ js/script.js
@@ -5,7 +5,10 @@ const btnLampadina = document.getElementById('btn-lamp')
 btnLampadina.addEventListener('click',function(){
     if(lampadina.src.includes('yellow')){
         lampadina.src = './img/white_lamp.png';
+        btnLampadina.innerText = 'SPEGNI'
     }
 
-    else(lampadina.src = './img/yellow_lamp.png')
+    else{lampadina.src = './img/yellow_lamp.png'
+         btnLampadina.innerText = 'ACCENDI'
+    }
 });
