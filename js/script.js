f28eec84a9bbf0c79d984994e628b1fe7c3072a7
MicheleCasani <casanimichele@gmail.com>
2025-03-25 14:31:41 +0100

    [UPD] add comment in js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 //creo le costanti 
 const lampadina = document.getElementById('lamp')
 const btnLampadina = document.getElementById('btn-lamp')
-
-btnLampadina.addEventListener('click',function(){
-    if(lampadina.src.includes('yellow')){
+//aggiungiamo l'eventualità del click
+btnLampadina.addEventListener('click', function () {
+    //qua gli diciamo che se l'immagine contiene la dicitura "yellow" allora la lampadina si "spegne" e si aggiorna la scritta su "SPEGNI" sul bottone
+    if (lampadina.src.includes('yellow')) {
         lampadina.src = './img/white_lamp.png';
         btnLampadina.innerText = 'SPEGNI'
     }
+    //altrimenti gli diciamo che se l'immagine non contiene la dicitura "yellow" l'immagine torna "normale" e appare la dicitua "ACCENDI" sul bottone
 
-    else{lampadina.src = './img/yellow_lamp.png'
-         btnLampadina.innerText = 'ACCENDI'
+    else {
+        lampadina.src = './img/yellow_lamp.png'
+        btnLampadina.innerText = 'ACCENDI'
     }
 });
